12a84ffdfc949f49a9b40ca57121a2765ce905c1
drisyaathul <drisyak809@gmail.com>
2023-05-13 03:06:18 +0800

    REFACTORED: Code to take the Mood message in Constructor

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,8 +1,25 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
+    /*
+     Refactor the code to take the mood message in Constructor.
+     */
+    private String message;
 
-    public String analyseMood(String message) {
+    public MoodAnalyser() {
+        /*
+         Default Constructor
+         */
+    }
+
+    public MoodAnalyser(String message) {
+        /*
+         Parameterised Constructor
+         */
+        this.message = message;
+    }
+
+    public String analyseMood() {
         /*
          * UC1: Call Analyse Mood function with message as parameter and return Happy or Sad mood.
          */
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -4,27 +4,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
-
-    static MoodAnalyser moodAnalyser = new MoodAnalyser();
     /*
-     * UC1: Call Analyse Mood function with message as parameter and return Happy or Sad mood.
+     * Refactor: analyseMood method will change to support no parameters and use message Field defined for the Class.
      */
     @Test
     public void givenMessageWhenMoodResultsSad() {
         /*
          * TC1.1 : analyseMood method can just return SAD to pass this Test Case
          */
-        String myMood = moodAnalyser.analyseMood("I am in Sad Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+        String myMood = moodAnalyser.analyseMood();
         Assertions.assertEquals("SAD",myMood);
         System.out.println(myMood);
     }
-
     @Test
     public void givenMessageWhenMoodResultHappy() {
         /*
          * TC1.2 : analyseMood method can just return HAPPY to pass this Test Case
          */
-        String myMood = moodAnalyser.analyseMood(("I am in Any Mood"));
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any Mood");
+        String myMood = moodAnalyser.analyseMood();
         Assertions.assertEquals("HAPPY",myMood);
         System.out.println(myMood);
     }
